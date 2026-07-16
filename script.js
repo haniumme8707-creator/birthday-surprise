bc5d5c7c4eb9e3226aba0e1cd60a760ad1704217
haniumme8707-creator <haniumme8707@gmail.com>
2026-07-16 05:32:43 +0530

    Add password protection system with loader
    This script implements a password protection system with a loader and error handling.

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+/* ==========================
+   PASSWORD SYSTEM
+========================== */
 
+const loader = document.getElementById("loader");
+const passwordScreen = document.getElementById("password-screen");
+const website = document.getElementById("website");
+const error = document.getElementById("error");
+
+website.style.display = "none";
+
+/* Loader */
+
+window.onload = function () {
+
+setTimeout(() => {
+
+loader.style.display = "none";
+
+}, 3500);
+
+};
+
+
+/* Password */
+
+function checkPassword() {
+
+const password = document
+.getElementById("password")
+.value
+.trim();
+
+if(password === "09092024"){
+
+passwordScreen.style.display = "none";
+
+website.style.display = "block";
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
+
+else{
+
+error.innerHTML = "Wrong Password 🤍";
+
+error.style.color = "#ff8b8b";
+
+error.style.marginTop = "15px";
+
+}
+
+}
